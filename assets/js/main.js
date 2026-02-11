8cd2d242b412889b192a4a500de67051f3441150
Johann Cardenas <johannc2@illinois.edu>
2026-02-11 00:26:17 -0600

    <update>: hamburger menu icon fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,7 +36,11 @@
 		// Title Bar.
 			$(
 				'<div id="titleBar">' +
-					'<a href="#navPanel" class="toggle"></a>' +
+					'<a href="#navPanel" class="toggle" aria-label="Open menu">' +
+						'<span class="toggle-icon" aria-hidden="true">' +
+							'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="22" height="22" fill="none" stroke="currentColor" stroke-width="2.25" stroke-linecap="round" stroke-linejoin="round"><line x1="3" y1="6" x2="21" y2="6"/><line x1="3" y1="12" x2="21" y2="12"/><line x1="3" y1="18" x2="21" y2="18"/></svg>' +
+						'</span>' +
+					'</a>' +
 				'</div>'
 			)
 				.appendTo($body);
